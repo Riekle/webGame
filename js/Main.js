2b94d3c9e81db6c1280d671ee91681860b6a88a3
Riekle Siersma <rsiersma@gmail.com>
2015-05-18 23:09:32 +0200

    Basic scene + moving cube

--- js/Main.js
+++ js/Main.js
@@ -1,6 +1,18 @@
+// Declare vars
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var cube = new THREE.Mesh( geometry, material );
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+// Add element to html body
 document.body.appendChild( renderer.domElement );
+
+// Renderer shit
+renderer.setSize( window.innerWidth, window.innerHeight );
+
+// Scene stuff
+scene.add( cube );
+
+// Camera stuff
+camera.position.z = 5;
